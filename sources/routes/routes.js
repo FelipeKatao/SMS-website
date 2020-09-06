5176d13db26e1951082962fde77dfdb9b291dfd2
Felipe Catão do Nascimento <cataofelipe@gmail.com>
2020-09-05 23:10:37 -0300

    update

--- sources/routes/routes.js
+++ sources/routes/routes.js
@@ -4,9 +4,9 @@ import mainPage from "../components/carrousel.js"
 let eng = new EngineApp()
 
 function ManageRoutes(){
-    eng.routesEngine.registerRoute("http://127.0.0.1:5500/",mainPage)
+    eng.routesEngine.registerRoute("https://felipekatao.github.io/SMS-website/",mainPage)
     eng.routesEngine.registerRoute("https://felipekatao.github.io/SMS-website/index.htmlo#home",mainPage)
-    eng.routesEngine.runRoute("https://felipekatao.github.io/SMS-website/","https://felipekatao.github.io/SMS-website/index.htmlo#home")
+    eng.routesEngine.runRoute("https://felipekatao.github.io/SMS-website/","https://felipekatao.github.io/SMS-website/index.html#home")
 }
 
 
