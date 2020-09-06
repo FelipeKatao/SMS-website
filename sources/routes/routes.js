560be3d03b55ebf393dd32cef6aa8e215563727a
Felipe Catão do Nascimento <cataofelipe@gmail.com>
2020-09-05 23:03:23 -0300

    update page for gitHub io

--- sources/routes/routes.js
+++ sources/routes/routes.js
@@ -5,8 +5,8 @@ let eng = new EngineApp()
 
 function ManageRoutes(){
     eng.routesEngine.registerRoute("http://127.0.0.1:5500/",mainPage)
-    eng.routesEngine.registerRoute("http://127.0.0.1:5500/index.html#",mainPage)
-    eng.routesEngine.runRoute("http://127.0.0.1:5500/","http://127.0.0.1:5500/index.html#")
+    eng.routesEngine.registerRoute("https://felipekatao.github.io/SMS-website/index.htmlo#home",mainPage)
+    eng.routesEngine.runRoute("https://felipekatao.github.io/SMS-website/","https://felipekatao.github.io/SMS-website/index.htmlo#home")
 }
 
 
