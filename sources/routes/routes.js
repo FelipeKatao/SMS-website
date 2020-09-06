5528c7d580a796021aa4b98bc27672b8c176e853
Felipe Catão do Nascimento <cataofelipe@gmail.com>
2020-09-05 23:19:45 -0300

    change route register

--- sources/routes/routes.js
+++ sources/routes/routes.js
@@ -5,7 +5,7 @@ let eng = new EngineApp()
 
 function ManageRoutes(){
     eng.routesEngine.registerRoute("https://felipekatao.github.io/SMS-website/",mainPage)
-    eng.routesEngine.registerRoute("https://felipekatao.github.io/SMS-website/index.htmlo#home",mainPage)
+    eng.routesEngine.registerRoute("https://felipekatao.github.io/SMS-website/index.html#home",mainPage)
     eng.routesEngine.runRoute("https://felipekatao.github.io/SMS-website/","https://felipekatao.github.io/SMS-website/index.html#home")
 }
 
