2269998c27429f5cca0caac74a4252fd21d8faa7
Felipe Catão do Nascimento <cataofelipe@gmail.com>
2020-09-16 02:08:11 -0300

    change find icons and placeholder

--- sources/components/menu.js
+++ sources/components/menu.js
@@ -33,8 +33,8 @@ function menuRender(){
          "<a class='nav-link' href='#'>Contato</a>"+
       "</ul>"+
       "<form class='form-inline my-2 my-lg-0'>"+
-        "<input class='form-control mr-sm-2' type='search' placeholder='Search' aria-label='Search'>"+
-        "<button class='btn btn-outline-success my-2 my-sm-0' type='submit'>Search</button>"+
+        "<input class='form-control mr-sm-2' type='search' placeholder='Procure por algo...' aria-label='Search'>"+
+        "<button class='btn btn-outline-success my-2 my-sm-0' type='submit'><img src='sources/img/lupa.svg'></button>"+
       "</form>"+
     "</div>"+
   "</nav>"
