1eee8ad98e9ffdce1bfeb208354d76df018d0399
Felipe Catão do Nascimento <cataofelipe@gmail.com>
2020-09-23 23:57:55 -0300

    new style for site

--- sources/components/footer.js
+++ sources/components/footer.js
@@ -9,7 +9,7 @@ function footer(){
 function footerRender(){
     return "<br><footer>"+
         "<div class='row'>"+
-          "<div class='col-md-6 mt-md-0 mt-3'>"+
+          "<div id='aboutFooter' class='col-md-6 mt-md-0 mt-3'>"+
             "<h5 class='text-uppercase'>Sms pratico</h5>"+
             "<p>Plataforma educacional sobre segurança do trabalho e meio ambiente.</p>"+
           "</div>"+
@@ -31,26 +31,9 @@ function footerRender(){
               "</li>"+
             "</ul>"+
           "</div>"+
-          "<div class='col-md-3 mb-md-0 mb-3'>"+
-            "<h5 class='text-uppercase'>Parceiros</h5>"+
-            "<ul class='list-unstyled'>"+
-              "<li>"+
-                "<a href='#!'>Link 1</a>"+
-              "</li>"+
-              "<li>"+
-                "<a href='#!'>Link 2</a>"+
-              "</li>"+
-              "<li>"+
-                "<a href='#!'>Link 3</a>"+
-              "</li>"+
-              "<li>"+
-                "<a href='#!'>Link 4</a>"+
-              "</li>"+
-            "</ul>"+
-          "</div>"+
         "</div>"+
       "</div>"+
-      "<div class='footer-copyright text-center py-3'>© 2020 Copyright:"+
+      "<div id='corp' class='footer-copyright text-center py-3'>© 2020 Copyright:"+
         "<a href='https://github.com/FelipeKatao'> Felipe Katao </a>"+
       "</div>"+
     "</footer>"
