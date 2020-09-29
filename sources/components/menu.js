878e49ad49e2ecf84638663d0516951eb01e2f04
Felipe Catão do Nascimento <cataofelipe@gmail.com>
2020-09-29 13:49:17 -0300

    menu create new element

--- sources/components/menu.js
+++ sources/components/menu.js
@@ -27,9 +27,9 @@ function menuRender(){
     "<button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarTogglerDemo01' aria-controls='navbarTogglerDemo01' aria-expanded='false' aria-label='Toggle navigation'>"+
       "<span><img src='sources/img/logo.png' height='40vh'></span>"+
     "</button>"+
-    "<div class='collapse navbar-collapse' id='navbarTogglerDemo01'>"+
+    "<div id='menu-content' class='collapse navbar-collapse' id='navbarTogglerDemo01'>"+
       "<a class='navbar-brand' href='#'><img id='logoMain' src='sources/img/logo.png' height='40vh'></a>"+
-      "<ul class='navbar-nav mr-auto mt-2 mt-lg-0 padingNavItem'>"+
+      "<ul id='menu-list' class='navbar-nav mr-auto mt-2 mt-lg-0 padingNavItem'>"+
         "<li class='nav-item'>"+
          " <a class='nav-link' href='#'>Segurança</a>"+
         "</li>"+
@@ -48,6 +48,7 @@ function menuRender(){
         "<input id='searchBox1' class='form-control mr-sm-2 searchBox-disible' type='search' placeholder='Procure por algo...' aria-label='Search'>"+
         "<button id='searchbt' class='btn my-2 my-sm-0' style='background-color:white;'><img src='sources/img/lupa.svg'></button>"+
     "</div>"+
+    "<div class='grayNav'></div>"+
   "</nav>"
 }
 
